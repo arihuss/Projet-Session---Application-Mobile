6f756650216f4f3eadf4c2e4e5266963fba933f1
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2025-03-26 23:28:06 -0400

    set up log in

--- RAY/app/src/main/java/com/yassine_roma_ariane/ray/modeles/CompteUtilisateur.java
+++ RAY/app/src/main/java/com/yassine_roma_ariane/ray/modeles/CompteUtilisateur.java
@@ -1,4 +1,71 @@
 package com.yassine_roma_ariane.ray.modeles;
 
 public class CompteUtilisateur {
+    private int id, age;
+    private String nom, prenom, email, mdp, telephone, adresse;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
 }
--- RAY/app/src/main/java/com/yassine_roma_ariane/ray/vues/LogInActivity.java
+++ RAY/app/src/main/java/com/yassine_roma_ariane/ray/vues/LogInActivity.java
@@ -1,6 +1,11 @@
 package com.yassine_roma_ariane.ray.vues;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +15,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.yassine_roma_ariane.ray.R;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtCourriel;
+    private EditText edtMdp;
+    private Button btnSeConnecter;
+    private TextView txtCreerCompte;
+    private TextView txtPasDeCompte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        edtCourriel=findViewById(R.id.edtCourriel);
+        edtMdp=findViewById(R.id.edtMdp);
+        btnSeConnecter=findViewById(R.id.btnSeConnecter);
+        txtCreerCompte=findViewById(R.id.txtCreerCompte);
+        txtPasDeCompte=findViewById(R.id.txtPasDeCompte);
+        btnSeConnecter.setOnClickListener(this);
+        txtPasDeCompte.setOnClickListener(this);
+        txtCreerCompte.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==btnSeConnecter){
+
+        }
+        if (v==txtCreerCompte || v==txtPasDeCompte){
+            Intent intent = new Intent(LogInActivity.this, SignInActivity.class);
+            startActivity(intent);
+        }
     }
 }
