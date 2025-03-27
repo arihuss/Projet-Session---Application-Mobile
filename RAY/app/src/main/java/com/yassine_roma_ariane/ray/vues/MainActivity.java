c198864005f9f0146929cf699aa62b1059b32509
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2025-03-26 21:57:43 -0400

    nav bar

--- RAY/app/src/main/java/com/yassine_roma_ariane/ray/vues/MainActivity.java
+++ RAY/app/src/main/java/com/yassine_roma_ariane/ray/vues/MainActivity.java
@@ -1,13 +1,17 @@
 package com.yassine_roma_ariane.ray.vues;
 
+import static androidx.navigation.Navigation.findNavController;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.yassine_roma_ariane.ray.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,12 +19,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        if (savedInstanceState == null){
+            Intent intent = new Intent(this, LogInActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                // Vérification des éléments sélectionnés
+                if (item.getItemId() == R.id.accueilFragment) {
+                    selectedFragment = new AccueilFragment();
+                } else if (item.getItemId() == R.id.historiqueFragment) {
+                    selectedFragment = new HistoriqueFragment();
+                } else if (item.getItemId() == R.id.deconnexion) {
+                    Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+                    startActivity(intent);
+                    finish(); // Ferme MainActivity après déconnexion
+                    return true;
+                }
+
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment, selectedFragment)
+                            .commit();
+                }
+                return true;
+            }
         });
     }
 }
