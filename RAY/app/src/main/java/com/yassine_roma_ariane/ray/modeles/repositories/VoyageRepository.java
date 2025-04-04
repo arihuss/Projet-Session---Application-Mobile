91261584528cc76867f9ec8547a8dfabc52ee367
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2025-04-03 20:53:32 -0400

    backend voyages

--- RAY/app/src/main/java/com/yassine_roma_ariane/ray/modeles/repositories/VoyageRepository.java
+++ RAY/app/src/main/java/com/yassine_roma_ariane/ray/modeles/repositories/VoyageRepository.java
@@ -1,4 +1,50 @@
 package com.yassine_roma_ariane.ray.modeles.repositories;
 
+
+import com.yassine_roma_ariane.ray.modeles.CompteUtilisateur;
+import com.yassine_roma_ariane.ray.modeles.Voyage;
+import com.yassine_roma_ariane.ray.modeles.dao.CompteUtilisateurDAO;
+import com.yassine_roma_ariane.ray.modeles.dao.VoyageDAO;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.List;
+
 public class VoyageRepository {
+
+    public interface FetchVoyageCallback {
+        void onFetch(List<Voyage> voyages);
+        void onError(String errorMessage);
+    }
+
+    public interface FetchVoyageByFilterCallback {
+        void onFetch(List<Voyage> voyages);
+        void onError(String errorMessage);
+    }
+
+    public void fetchVoyagesFromAPI(VoyageRepository.FetchVoyageCallback callback){
+        new Thread(()-> {
+            try {
+                List<Voyage> voyages = VoyageDAO.getVoyages();
+                callback.onFetch(voyages);
+            }catch (IOException | JSONException e){
+                callback.onError("Erreur: " + e.getMessage());
+            }
+        }).start();
+    }
+
+    public void fetchVoyagesByFilter(String destination,
+                                     String type, String date,
+                                     double budget,
+                                     VoyageRepository.FetchVoyageByFilterCallback callback){
+        new Thread(()-> {
+            try {
+                List<Voyage> voyages = VoyageDAO.getVoyagesByFilter(destination,type,date, budget);
+                callback.onFetch(voyages);
+            }catch (IOException | JSONException e){
+                callback.onError("Erreur: " + e.getMessage());
+            }
+        }).start();
+    }
 }
--- RAY/app/src/main/java/com/yassine_roma_ariane/ray/viewModel/VoyageViewModel.java
+++ RAY/app/src/main/java/com/yassine_roma_ariane/ray/viewModel/VoyageViewModel.java
@@ -1,4 +1,59 @@
 package com.yassine_roma_ariane.ray.viewModel;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.yassine_roma_ariane.ray.modeles.Voyage;
+
+import com.yassine_roma_ariane.ray.modeles.repositories.VoyageRepository;
+
+import java.util.List;
+
 public class VoyageViewModel {
+
+    private VoyageRepository repository;
+
+    private final MutableLiveData<List<Voyage>> voyagesMutable = new MutableLiveData<>();
+    private final MutableLiveData<List<Voyage>> voyagesFiltre = new MutableLiveData<>();
+    private final MutableLiveData<String> messageMutable = new MutableLiveData<>();
+
+    public VoyageViewModel() {
+        repository = new VoyageRepository();
+        // Chargement initial des comptes
+        refreshVoyages();
+    }
+
+    public LiveData<List<Voyage>> getVoyages(){return voyagesMutable;}
+
+    public LiveData<String> getMessage() {return messageMutable;}
+
+    public void filterVoyages(String destination, String type, String date, double budget) {
+        repository.fetchVoyagesByFilter(destination, type, date, budget, new VoyageRepository.FetchVoyageByFilterCallback() {
+            @Override
+            public void onFetch(List<Voyage> voyages) {
+                voyagesMutable.postValue(voyages);
+            }
+            @Override
+            public void onError(String errorMessage) {
+                messageMutable.postValue(errorMessage);
+            }
+        });
+    }
+
+
+    public void refreshVoyages(){
+        repository.fetchVoyagesFromAPI(new VoyageRepository.FetchVoyageCallback() {
+            @Override
+            public void onFetch(List<Voyage> voyages) {
+                voyagesMutable.postValue(voyages);
+            }
+            @Override
+            public void onError(String errorMessage) {
+                messageMutable.postValue(errorMessage);
+            }
+        });
+    }
+
+
+
 }
