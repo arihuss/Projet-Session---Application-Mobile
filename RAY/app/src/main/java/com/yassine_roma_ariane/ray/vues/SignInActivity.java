b3088760caae524c16d072e24807acde69594bc1
romadr199 <desruisseauxroma@gmail.com>
2025-04-04 08:52:26 -0400

    ui inscription

--- RAY/app/src/main/java/com/yassine_roma_ariane/ray/vues/SignInActivity.java
+++ RAY/app/src/main/java/com/yassine_roma_ariane/ray/vues/SignInActivity.java
@@ -1,6 +1,10 @@
 package com.yassine_roma_ariane.ray.vues;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +14,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.yassine_roma_ariane.ray.R;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText edtPrenom, edtNom, edtTelephone, edtAdresse, edtCourriel, edtMdp, edtMdpConfirme, edtAge;
+    private Button btnInscrire;
+    private TextView txtRetour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,32 @@ public class SignInActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Liaison avec la vue
+        edtPrenom = findViewById(R.id.edtPrenom);
+        edtNom = findViewById(R.id.edtNom);
+        edtCourriel = findViewById(R.id.edtCourriel);
+        edtTelephone = findViewById(R.id.edtTelephone);
+        edtAdresse = findViewById(R.id.edtAdresse);
+        edtAge = findViewById(R.id.edtAge);
+        edtMdp = findViewById(R.id.edtMdp);
+        edtMdpConfirme = findViewById(R.id.edtMdpConfirme);
+
+        btnInscrire = findViewById(R.id.btnInscrire);
+        txtRetour = findViewById(R.id.txtRetour);
+
+        // Mettre les écouteurs
+        btnInscrire.setOnClickListener(this);
+        txtRetour.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnInscrire) {
+
+        }
+        if(v == txtRetour) {
+            
+        }
     }
 }
